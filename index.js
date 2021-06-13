89b4ddab98e9e28ce84aebbc801ad3b88a74a640
Brian C Sparks <briancsparks@gmail.com>
2021-06-13 00:40:37 -0700

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-const debug       = require('./lib/debug');
+const include                 = require('./lib/include')(module);
+
+const config                  = require('./lib/config');
+const logging                 = require('./lib/logging');
+const debug                   = require('./lib/debug');
+const grumpy                  = require('./lib/grumpy');
+const status                  = require('./lib/status');
 
 module.exports = Object.assign({},
-    require('./lib/mod')
+    require('./lib/mod'),
+
+    {
+      logging,
+      debug,
+      grumpy,
+      config,
+      status
+    },
+
+    config
 );
